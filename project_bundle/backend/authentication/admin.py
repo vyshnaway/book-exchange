2d835203d149b173d168a89c92ed814e20a193cf
yshelldev <vyshnav.prasad@outlook.com>
2026-02-07 18:47:03 +0530

    chore: Remove a large number of static files, backend application components, and frontend development artifacts.

--- project_bundle/backend/authentication/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import BookReader, Notification, ProfileImage
-
-admin.site.register(BookReader)
-admin.site.register(ProfileImage)
-admin.site.register(Notification)
